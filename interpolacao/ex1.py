0547a22c4d0a55c4f1f09e5ea011414ecc09fc04
André Felipe Fuck <66512647+andreFelipeHFuck@users.noreply.github.com>
2022-09-27 09:39:27 -0300

    Add files via upload

---
+++ interpolacao/ex1.py
@@ -0,0 +1,35 @@
+# Interpolação
+
+import numpy as np 
+
+def vandermonde(x: list, y: list):
+    A = []
+    B = y
+    n = len(x)
+    for xi in x:
+        row = [1] + [xi ** k for k in range(1, n)]
+        A.append(row)
+    coeffs = np.linalg.solve(A, B)
+    return coeffs
+
+def build_poly(coeffs):
+    def func(x):
+        soma = coeffs[0]
+        for i, ci in enumerate(coeffs[1:]):
+            soma += ci * x ** i
+    return func
+
+
+if __name__ == "__main__":
+    n: int = 5
+    x = [1, 3]
+    def f(x: float) -> float:
+        return x**2
+    y = [f(xi) for xi in x]
+
+    coeffs = vandermonde(x, y)
+
+    p = build_poly(coeffs)
+
+    t = np.linspace(-1, 4, 100)
+    pt = [p(ti) for ti in t]
